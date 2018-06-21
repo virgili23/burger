b4914b78b7a9e7a9e09539588f4e7181e1b053ea
virgil23 <virgil17@live.com>
2018-06-20 22:23:41 -0400

    Added files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+--inserted some dummy values
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheesy chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('spicy chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ brisket', false);
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser')
+
+app.get('/', function (req, res) {
+
+    console.log("Listening on Port 3000");
+    
+    res.send('Hello World')
+  })
+   
+  app.listen(3000)

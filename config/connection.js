a3a97d51ad7bfd296b07846bfd82dd48edc9e34e
virgil23 <virgil17@live.com>
2018-06-20 22:23:13 -0400

    Added files

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "burgers_db"
+});
